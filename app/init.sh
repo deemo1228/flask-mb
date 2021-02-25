3a7988d3a7e82c4c8322f4c83f855205d039c723
deemo <deemo_1228@reddoor.com.tw>
2021-02-25 17:08:40 +0800

    first commit

--- app/init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -e
-
-echo "Running Development Server"
-flask run --host=0.0.0.0 --port=5000 --reload
--- app/views/frontend/__init__.py
+++
@@ -1,4 +0,0 @@
-from flask import Blueprint
-
-frontend = Blueprint('frontend', __name__)
-from . import main
